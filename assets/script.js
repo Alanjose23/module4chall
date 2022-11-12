e65b219bcf8d6d57b6d1b598393093c8cc736cc7
ALAN JOSE <alanjose04231120@gmail.com>
2022-11-12 17:48:25 -0500

    created and finished logic and added comments

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,7 @@ function generaterandomnum() {
 }
 function generatePassword(){
 // prompt user for password criteria
+// prompts followed by saving the data
 var passuess = window.prompt("do you want a new password", "yes");
 var length = window.prompt("What is your password length (has to be between 8-128)");
 var specpass = window.prompt("do you want special characters (yes or no)",'yes');
@@ -24,17 +25,21 @@ var numpass = window.prompt("do you want numbers in password (yes or no)", "yes"
 var upperpass = window.prompt("do you want uppercase characters (yes or no)", "yes");
 var lowerpass = window.prompt("do you want lowercase characters (yes or no)", "yes");
 if (passuess === 'yes' && length > 7 && length < 129) {
-
+// ser parameters to run the code
 if (specpass === "yes"&&numpass === "yes"&&upperpass === "yes"&&lowerpass === "yes") {
+// add all arrays together for addition
 generatorarr = lettersL.concat(lettersU).concat(numbers).concat(specialcar);
+// for loop to iterate and create password array
 for(var i = 0; i < length;i++){
 var password1 = generatorarr[generaterandomnum()];
 passwordarr.push(password1);
 }
+// create string password to return
 var finalpass = passwordarr.join('');
 return finalpass;
 }
 else if(specpass === "no"&&numpass === "yes"&&upperpass === "yes"&&lowerpass === "yes"){
+// based on paramters run and add arrays
 generatorarr = lettersL.concat(lettersU).concat(numbers);
 for(var i = 0; i < length;i++){
 var password1 = generatorarr[generaterandomnum()];
@@ -44,6 +49,7 @@ var finalpass = passwordarr.join('');
 return finalpass;
 }
 else if(specpass === "yes"&&numpass === "no"&&upperpass === "yes"&&lowerpass === "yes") {
+// based on paramters run and add arrays
     generatorarr = lettersL.concat(lettersU).concat(specialcar);
     for(var i = 0; i < length;i++){
     var password1 = generatorarr[generaterandomnum()];
@@ -53,6 +59,7 @@ else if(specpass === "yes"&&numpass === "no"&&upperpass === "yes"&&lowerpass ===
     return finalpass;
 }
 else if(specpass === "no"&&numpass === "no"&&upperpass === "yes"&&lowerpass === "yes") {
+// based on paramters run and add arrays
     generatorarr = lettersL.concat(lettersU);
     for(var i = 0; i < length;i++){
     var password1 = generatorarr[generaterandomnum()];
@@ -62,6 +69,7 @@ else if(specpass === "no"&&numpass === "no"&&upperpass === "yes"&&lowerpass === 
     return finalpass;
 }
 else if(specpass === "yes"&&numpass === "yes"&&upperpass === "no"&&lowerpass === "yes") {
+// based on paramters run and add arrays
     generatorarr = lettersL.concat(numbers).concat(specialcar);
     for(var i = 0; i < length;i++){
     var password1 = generatorarr[generaterandomnum()];
@@ -71,6 +79,7 @@ else if(specpass === "yes"&&numpass === "yes"&&upperpass === "no"&&lowerpass ===
     return finalpass;
 }
 else if(specpass === "yes"&&numpass === "yes"&&upperpass === "yes"&&lowerpass === "no") {
+// based on paramters run and add arrays
     generatorarr = lettersU.concat(numbers).concat(specialcar);
     for(var i = 0; i < length;i++){
     var password1 = generatorarr[generaterandomnum()];
@@ -80,6 +89,7 @@ else if(specpass === "yes"&&numpass === "yes"&&upperpass === "yes"&&lowerpass ==
     return finalpass;
 }
 else if(specpass === "yes"&&numpass === "yes"&&upperpass === "no"&&lowerpass === "no") {
+// based on paramters run and add arrays
     generatorarr = numbers.concat(specialcar);
     for(var i = 0; i < length;i++){
     var password1 = generatorarr[generaterandomnum()];
@@ -89,6 +99,8 @@ else if(specpass === "yes"&&numpass === "yes"&&upperpass === "no"&&lowerpass ===
     return finalpass;
 }
 else if(specpass === "no"&&numpass === "yes"&&upperpass === "yes"&&lowerpass === "no") {
+// based on paramters run and add arrays
+
     generatorarr = lettersU.concat(numbers);
     for(var i = 0; i < length;i++){
     var password1 = generatorarr[generaterandomnum()];
@@ -98,6 +110,7 @@ else if(specpass === "no"&&numpass === "yes"&&upperpass === "yes"&&lowerpass ===
     return finalpass;
 }
 else if(specpass === "yes"&&numpass === "no"&&upperpass === "no"&&lowerpass === "yes") {
+// based on paramters run and add arrays
     generatorarr = lettersL.concat(specialcar);
     for(var i = 0; i < length;i++){
     var password1 = generatorarr[generaterandomnum()];
@@ -107,6 +120,7 @@ else if(specpass === "yes"&&numpass === "no"&&upperpass === "no"&&lowerpass === 
     return finalpass;
 }
 else if(specpass === "no"&&numpass === "no"&&upperpass === "no"&&lowerpass === "yes") {
+// based on paramters run and add arrays
     generatorarr = lettersL;
     for(var i = 0; i < length;i++){
     var password1 = generatorarr[generaterandomnum()];
@@ -116,6 +130,7 @@ else if(specpass === "no"&&numpass === "no"&&upperpass === "no"&&lowerpass === "
     return finalpass;
 }
 else if(specpass === "yes"&&numpass === "no"&&upperpass === "no"&&lowerpass === "no") {
+// based on paramters run and add arrays
     generatorarr = specialcar;
     for(var i = 0; i < length;i++){
     var password1 = generatorarr[generaterandomnum()];
@@ -125,6 +140,7 @@ else if(specpass === "yes"&&numpass === "no"&&upperpass === "no"&&lowerpass === 
     return finalpass;
 }
 else if(specpass === "no"&&numpass === "yes"&&upperpass === "no"&&lowerpass === "no"){
+// based on paramters run and add arrays
     generatorarr = numbers;
     for(var i = 0; i < length;i++){
     var password1 = generatorarr[generaterandomnum()];
@@ -134,6 +150,7 @@ else if(specpass === "no"&&numpass === "yes"&&upperpass === "no"&&lowerpass === 
     return finalpass;
 }
 else if(specpass === "no"&&numpass === "no"&&upperpass === "yes"&&lowerpass === "no") {
+// based on paramters run and add arrays
     generatorarr = lettersU;
     for(var i = 0; i < length;i++){
     var password1 = generatorarr[generaterandomnum()];
@@ -143,6 +160,7 @@ else if(specpass === "no"&&numpass === "no"&&upperpass === "yes"&&lowerpass === 
     return finalpass;
 }
 else if(specpass === "yes"&&numpass === "no"&&upperpass === "yes"&&lowerpass === "no"){
+// based on paramters run and add arrays
     generatorarr = lettersU.concat(specialcar);
     for(var i = 0; i < length;i++){
     var password1 = generatorarr[generaterandomnum()];
@@ -152,6 +170,7 @@ else if(specpass === "yes"&&numpass === "no"&&upperpass === "yes"&&lowerpass ===
     return finalpass;
 }
 else if(specpass === "no"&&numpass === "yes"&&upperpass === "no"&&lowerpass === "yes"){
+// based on paramters run and add arrays
     generatorarr = lettersL.concat(numbers);
     for(var i = 0; i < length;i++){
     var password1 = generatorarr[generaterandomnum()];
@@ -161,10 +180,12 @@ else if(specpass === "no"&&numpass === "yes"&&upperpass === "no"&&lowerpass === 
     return finalpass;
 }
 else{
+// for no parameters
     window.alert("please reenter prompts, password is invalid")
 }
 }
 else {
+// for no parameters and no selection for password prompt
     window.alert("please select yes for the new password prompt");
 }
 // password length 8-128
